fdd2b4afc608a9026ae5945fd3df2c31487e161d
Vadym <vadym1927dk@gmail.com>
2024-11-14 10:03:25 +0200

    Зміна кольору текста на виводі

--- YourLuckyNumber/Program.cs
+++ YourLuckyNumber/Program.cs
@@ -11,7 +11,9 @@
             Random random = new();
             sbyte luckyNumber = (sbyte)random.Next(0,99); //генерація рандомного числа від 0 до 99
 
+            Console.ForegroundColor = ConsoleColor.Cyan; // зміна кольору тескту в консолі
             Console.WriteLine($"Сьогодні твоє щасливе число {luckyNumber}");
+            Console.ResetColor();
         }
     }
 }
